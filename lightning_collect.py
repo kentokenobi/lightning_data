239a482b847d683eb367b9d45c4f467bd18054f3
kentokenobi <kento.tanaka@nex.nikkei.com>
2024-08-07 10:56:40 +0900

    update_code

---
+++ lightning_collect.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import geopandas as gpd
+import requests
+import time
+import datetime
+from io import BytesIO
+
+today = f'{uk_time.year}' + f'{uk_time.month:02}' + f'{uk_time.day:02}'
+today_csv = f'/Users/kento-tanaka/Documents/取材関連/気候変動/雷/Github/lightning_{today}.csv'
+
+# Exist file
+try:
+    df_base = pd.read_csv(today_csv)
+    
+    uk_time = datetime.datetime.now() - datetime.timedelta(hours=9)
+    five_min = (uk_time.minute // 5) * 5 - 5
+    current_time = f'{uk_time.year}' + f'{uk_time.month:02}' + f'{uk_time.day:02}' + f'{uk_time.hour:02}' + f'{five_min:02}'
+    url = f'https://www.jma.go.jp/bosai/jmatile/data/nowc/{current_time}00/none/{current_time}00/surf/liden/data.geojson?id=liden'
+    response = requests.get(url)
+    gdf = gpd.read_file(BytesIO(response.content))
+    df_base = pd.concat([df_base, gdf], axis=0)
+    df_base.to_csv(today_csv)
+
+# Not exist
+else:
+    df_base = pd.DataFrame(today_csv)
+    
+    uk_time = datetime.datetime.now() - datetime.timedelta(hours=9)
+    five_min = (uk_time.minute // 5) * 5 - 5
+    current_time = f'{uk_time.year}' + f'{uk_time.month:02}' + f'{uk_time.day:02}' + f'{uk_time.hour:02}' + f'{five_min:02}'
+    url = f'https://www.jma.go.jp/bosai/jmatile/data/nowc/{current_time}00/none/{current_time}00/surf/liden/data.geojson?id=liden'
+    response = requests.get(url)
+    gdf = gpd.read_file(BytesIO(response.content))
+    df_base = pd.concat([df_base, gdf], axis=0)
+    df_base.to_csv(today_csv)
+
