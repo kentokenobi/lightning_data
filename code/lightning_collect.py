925d75205eef2871ce344a263979476df19ec05e
kentokenobi <kento.tanaka@nex.nikkei.com>
2024-08-07 11:27:47 +0900

    commit

--- code/lightning_collect.py
+++ code/lightning_collect.py
@@ -1,36 +1,34 @@
 import pandas as pd
 import geopandas as gpd
 import requests
-import time
 import datetime
 from io import BytesIO
 
+# UK time
+uk_time = datetime.datetime.now() - datetime.timedelta(hours=9)
 today = f'{uk_time.year}' + f'{uk_time.month:02}' + f'{uk_time.day:02}'
 today_csv = f'data/lightning_{today}.csv'
 
 # Exist file
 try:
     df_base = pd.read_csv(today_csv)
-    
-    uk_time = datetime.datetime.now() - datetime.timedelta(hours=9)
-    five_min = (uk_time.minute // 5) * 5 - 5
-    current_time = f'{uk_time.year}' + f'{uk_time.month:02}' + f'{uk_time.day:02}' + f'{uk_time.hour:02}' + f'{five_min:02}'
-    url = f'https://www.jma.go.jp/bosai/jmatile/data/nowc/{current_time}00/none/{current_time}00/surf/liden/data.geojson?id=liden'
-    response = requests.get(url)
-    gdf = gpd.read_file(BytesIO(response.content))
-    df_base = pd.concat([df_base, gdf], axis=0)
-    df_base.to_csv(today_csv)
+except FileNotFoundError:
+    df_base = pd.DataFrame()
 
-# Not exist
-else:
-    df_base = pd.DataFrame(today_csv)
+# Fetch new data
+five_min = (uk_time.minute // 5) * 5 - 5
+if five_min < 0:
+    five_min = 55
+    uk_time = uk_time - datetime.timedelta(hours=1)
     
-    uk_time = datetime.datetime.now() - datetime.timedelta(hours=9)
-    five_min = (uk_time.minute // 5) * 5 - 5
-    current_time = f'{uk_time.year}' + f'{uk_time.month:02}' + f'{uk_time.day:02}' + f'{uk_time.hour:02}' + f'{five_min:02}'
-    url = f'https://www.jma.go.jp/bosai/jmatile/data/nowc/{current_time}00/none/{current_time}00/surf/liden/data.geojson?id=liden'
-    response = requests.get(url)
-    gdf = gpd.read_file(BytesIO(response.content))
-    df_base = pd.concat([df_base, gdf], axis=0)
-    df_base.to_csv(today_csv)
+current_time = f'{uk_time.year}' + f'{uk_time.month:02}' + f'{uk_time.day:02}' + f'{uk_time.hour:02}' + f'{five_min:02}'
+url = f'https://www.jma.go.jp/bosai/jmatile/data/nowc/{current_time}00/none/{current_time}00/surf/liden/data.geojson?id=liden'
+response = requests.get(url)
+gdf = gpd.read_file(BytesIO(response.content))
 
+# Concatenate the data
+if not gdf.empty:
+    df_base = pd.concat([df_base, gdf], axis=0)
+
+# Save the file
+df_base.to_csv(today_csv, index=False)
